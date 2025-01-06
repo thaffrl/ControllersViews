da110882174bc6e80e416338f507e98d94d17112
thaffrl <althaffarrelbusiness@gmail.com>
2025-01-06 23:55:27 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,52 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\RouteController; 
+use App\Http\Controllers\HomeController; 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EmployeeController;
 
-Route::get('/routing', function () {
-    return view('routing');
-});
-Route::get('/basic_routing', function() { 
-    return 'Hello World'; 
-}); 
-Route::view('/view_route', 'view_route'); 
-Route::view('/view_route', 'view_route', ['name' => 'Purnama']); 
-Route::get('/controller_route', [RouteController::class, 'index']);
-Route::redirect('/', '/routing');
-Route::get('/user/{id}', function($id) { 
-    return "User Id: ".$id; 
-}); 
-Route::get('/posts/{post}/comments/{comment}', function($postId, 
-$commentId) { 
-    return "Post Id: ".$postId.", Comment Id: ".$commentId; 
-}); 
-Route::get('username/{name?}', function($name = null) { 
-    return 'Username: '.$name; 
-    });
-    Route::get('/title/{title}', function($title) { 
-        return "Title: ".$title; 
-        })->where('title', '[A-Za-z]+');
-        Route::get('/profile/{profileId}', [RouteController::class, 'profile'])
-->name('profileRouteName');
 
-Route::get('/route_priority/user', function() { 
-    return "This is Route 1"; 
-}); 
-Route::get('/route_priority/user', function() { 
-    return "This is Route 2"; 
-}); 
-Route::fallback(function() { 
-    return 'This is Fallback Route'; 
-});
-Route::prefix('admin')->name('admin.')->group(function() { 
-    Route::get('/dashboard', function() { 
-        return "This is admin dashboard"; 
-    })->name('dashboard'); 
-    Route::get('/users', function() { 
-        return "This is user data on admin page"; 
-    })->name('users'); 
-    Route::get('/items', function() { 
-        return "This is item data on admin page"; 
-    })->name('items'); 
-});
-Route::view('/lynxc', 'lynxc')->name('lynxc');
+
+
+Route::get('home', [HomeController::class, 'index'])->name('home');
+Route::get('profile', ProfileController::class)->name('profile'); 
+Route::resource('employees', EmployeeController::class);
